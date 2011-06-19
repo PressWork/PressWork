11299f91c72acefcf0948199b37dc5f59e33270d
Brendan Sera-Shriar <brendan@vanillaforums.com>
2011-06-19 09:41:49 -0400

    Updated PressWork logo files and screenshot to new logo and modified admin-styles.css

--- presswork/admin/inc/welcome.php
+++ presswork/admin/inc/welcome.php
@@ -3,7 +3,7 @@
 function pw_welcome_screen() {
 	?>
 	<div id="pw_welcome_screen">
-        <img src="<?php echo get_template_directory_uri(); ?>/admin/images/pw-logo.jpg" width="260" height="114" alt="" class="pw-logo" />
+        <img src="<?php echo get_template_directory_uri(); ?>/admin/images/pw-logo.jpg" width="557" height="125" alt="" class="pw-logo" />
 		<a href="javascript:void(0)" id="close-welcome">Close</a>
 		<div class="close-arrow"></div>
 	</div>
